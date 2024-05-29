2d5bdedfabb4f93e9f9075360ce52d4e2609ee46
juliannas13 <jsanchezm@fi.uba.ar>
2024-05-29 02:12:12 -0300

    Creacion de links de acceso para las paginas, card id 6656aa7f5c4973f545c95574

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/habitaciones')
+def habitaciones():
+    return render_template('habitaciones.html')
+
+@app.route('/servicios')
+def servicios():
+    return render_template('servicios.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
