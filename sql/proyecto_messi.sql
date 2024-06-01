aa865452b90cecf3d681cdfe3bce43c67d7f4e83
RocioBustamante <rbustamante@fi.uba.ar>
2024-06-01 17:34:40 -0300

    Modifciación de tabla 'Tipos Habitaciones' (Card id 6657566189c87500c0ca0efa) y desarrollo de 'tipo_habitaciones.json' (Card id 66575f72a0364e062b4a8092)

--- sql/proyecto_messi.sql
+++ sql/proyecto_messi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 31-05-2024 a las 06:43:20
+-- Tiempo de generación: 01-06-2024 a las 22:31:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,19 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tipos_habitaciones` (
   `nombre` varchar(50) NOT NULL,
-  `especificaciones` varchar(150) NOT NULL,
-  `precio` decimal(7,2) NOT NULL
+  `especificaciones` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`especificaciones`)),
+  `precio` decimal(10,2) NOT NULL,
+  `ruta_imagen1` varchar(100) NOT NULL,
+  `ruta_imagen2` varchar(100) NOT NULL,
+  `ruta_imagen3` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `tipos_habitaciones`
 --
 
-INSERT INTO `tipos_habitaciones` (`nombre`, `especificaciones`, `precio`) VALUES
-('Ejecutiva', 'TV satélite, Minibar, Caja fuerte, WIFI, Aire Acondicionado, Habitación Climatizada, Microondas, Artículos de Aseo, Menaje de Cocina, Teléfono', 52000.00),
-('Suite Junior', 'TV satélite, Minibar, Caja fuerte, WIFI, Aire Acondicionado, Habitación Climatizada, Artículos de Aseo, Teléfono', 64000.00),
-('Suite Lujo', 'TV satélite, Minibar, Caja fuerte, Aire Acondicionado, Habitación Climatizada, Microondas, Artículos de Aseo, Bañera, Teléfono', 75000.00),
-('Twin', 'TV satélite, Minibar, Caja fuerte, WIFI, Aire Acondicionado, Habitación Climatizada, Microondas, Artículos de Aseo, Menaje de Cocina, Teléfono', 48000.00);
+INSERT INTO `tipos_habitaciones` (`nombre`, `especificaciones`, `precio`, `ruta_imagen1`, `ruta_imagen2`, `ruta_imagen3`) VALUES
+('Ejecutiva', '[\"TV LCD\", \"Minibar\", \"Caja Fuerte\", \"WIFI\", \"Cerradura Magnética\", \"Aire Acondicionado\", \"Habitación Climatizada\", \"Escritorio\", \"Microondas\", \"Artículos de Aseo\", \"Amenities\", \"Secador de Cabello\", \"Bañera\", \"Hidromasaje\", \"Teléfono\", \"Cafetera Nespresso\"]\r\n', 75000.00, '/static/images/Ejecutiva1.jpg', '/static/images/Ejecutiva2.jpg', '/static/images/Ejecutiva3.jpg'),
+('Suite Junior', '[\"TV LCD\",\"Microondas\", \"Caja fuerte\", \"Cerradura Magnética\", \"WIFI\", \"Aire Acondicionado\", \"Minibar\", \"Habitación Climatizada\", \"Escritorio\", \"Artículos de Aseo\",\"Amenities\", \"Secador de Cabello\", \"Bañera\", \"Teléfono\"]\r\n', 52000.00, '/static/images/SuiteJunior1.jpg', '/static/images/SuiteJunior2.jpg', ''),
+('Suite Lujo', '[\"TV LCD\", \"Minibar\", \"Aire Acondicionado\", \"WIFI\", \"Cerradura Magnética\", \"Habitación Climatizada\", \"Bañera\", \"Caja fuerte\", \"Escritorio\", \"Artículos de Aseo\", \"Amenities\", \"Secador de Cabello\", \"Jacuzzi\", \"Teléfono\"]\r\n', 64000.00, '/static/images/SuiteLujo1.jpg', '/static/images/SuiteLujo2.jpg', ''),
+('Twin', '[\"TV LCD\", \"Minibar\", \"Caja fuerte\", \"WIFI\", \"Cerradura Magnética\", \"Aire Acondicionado\", \"Habitación Climatizada\", \"Microondas\", \"Artículos de Aseo\", \"Amenities\", \"Secador de Cabello\", \"Bañera\", \"Teléfono\"]', 48000.00, '/static/images/Twin1.jpg', '/static/images/Twin2.jpg', '');
 
 --
 -- Índices para tablas volcadas
