5a0172b43d68f7dc92590216021f30b0119bd78c
JoelFiad <jfiad@fi.uba.ar>
2024-06-13 22:00:25 -0300

    Validaciones de datos en JS 6657ed2fc4c7247dec48705e

--- static/js/aviso.js
+++
@@ -1,6 +0,0 @@
-const aviso=document.getElementById("boton_enviar")
-
-aviso.addEventListener("click", function(event){
-    alert("¡Nos contactaremos muy pronto!")
-})
-
--- static/js/contacto.js
+++ static/js/contacto.js
@@ -1,17 +1,44 @@
 document.getElementById("formulario").addEventListener("submit", function(event) {
     event.preventDefault();
-    createUser();
+
+    const nameInput = document.getElementById("name").value;
+    const emailInput = document.getElementById("email").value;
+    const contactInput = document.getElementById("contact").value;
+    const messageInput = document.getElementById("message").value;
+
+    if (validateForm()) {
+        alert("¡Nos contactaremos muy pronto!");
+        createUser();
+    } else {
+        alert("Revise los valores ingresados en los campos.");
+    }
+
+    function validateForm() {
+        const formData = {
+            name: nameInput.trim(),
+            email: emailInput.trim(),
+            contact: contactInput.trim(),
+            message: messageInput.trim()
+        };
+    
+        return validateName(formData.name) && validateEmail(formData.email) && validateContact(formData.contact) && validateMessage(formData.message);
+    }
+    function validateName(name) {
+        return name.length > 0;
+    }
+    function validateEmail(email) {
+        return email.includes('@') && email.includes('.');
+    }
+    function validateContact(contact) {
+        return contact.split('').every(char => char >= '0' && char <= '9');
+    }
+    function validateMessage(message) {
+        return message.length > 0;
+    }
 });
 
 function createUser() {
-    let formData = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        contact: document.getElementById("contact").value,
-        message: document.getElementById("message").value
-    };
-
-    fetch('/create_user', {
+    fetch('/create_user', { //con fetch enviamos datos al server
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -25,7 +52,8 @@ function createUser() {
         return response.json();
     })
     .then(data => {
-        document.getElementById("aviso").innerText = data.message;
+        document.getElementById("aviso").innerText = data.message; 
+        document.getElementById("formulario").reset(); //Limpiamos el form porque ya mandamos los datos
     })
     .catch(error => {
         console.error('Error:', error);
