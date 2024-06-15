3cdbb49c5d99154ebefa9b6a4b4f02ee3e075030
RocioBustamante <rbustamante@fi.uba.ar>
2024-06-14 23:30:48 -0300

    Modificación de la lógica de la API en reservas (Card id 666b8631d1b21a888ad7c4ef)

--- BackEnd-API/app.py
+++ BackEnd-API/app.py
@@ -16,16 +16,48 @@ def set_connection():
     connection = engine.connect()
     return connection
 
+def verificar_disponibilidad_tipo(conn, roomtype):
+    query_disponibilidad = """
+    SELECT id_habitacion FROM habitaciones_disponibles 
+    WHERE nombre_habitacion = :roomtype
+    """
+    result_disponibilidad = conn.execute(text(query_disponibilidad), {'roomtype': roomtype}).fetchall()
+    return [row[0] for row in result_disponibilidad]
+
+def verificar_disponibilidad_fecha(conn, habitaciones_disponibles, checkin, checkout):
+    habitaciones_libres = []
+    for habitacion in habitaciones_disponibles:
+        query_reservas_existente = """
+        SELECT COUNT(*) FROM ocupacion_hotel 
+        WHERE id_habitacion = :habitacion 
+        AND (:checkout > fecha_checkin AND :checkin < fecha_checkout)
+        """
+        result_reservas_existente = conn.execute(text(query_reservas_existente), {'habitacion': habitacion, 'checkout': checkout, 'checkin': checkin}).scalar()
+        if result_reservas_existente == 0:
+            habitaciones_libres.append(habitacion)
+    return habitaciones_libres
+
 def insertar_cliente(conn, nombre, apellido, identificacion, telefono):
     query_insertar_cliente = """INSERT INTO cliente (nombre, apellido, identificacion, telefono) 
                             VALUES (:nombre, :apellido, :identificacion, :telefono)"""
     result_insertar_cliente = conn.execute(text(query_insertar_cliente), {'nombre': nombre, 'apellido': apellido, 'identificacion': identificacion, 'telefono': telefono})
     return result_insertar_cliente.lastrowid
 
+def insertar_ocupacion(conn, id_habitacion, checkin, checkout):
+    query_insertar_ocupacion = """INSERT INTO ocupacion_hotel (id_habitacion, fecha_checkin, fecha_checkout) 
+                            VALUES (:id_habitacion, :checkin, :checkout)"""
+    conn.execute(text(query_insertar_ocupacion), {'id_habitacion': id_habitacion, 'checkin': checkin, 'checkout': checkout})
+
+def insertar_reserva(conn, id_cliente, id_habitacion, roomtype, checkin, checkout, persons, bedtype):
+    query_insertar_reserva = """INSERT INTO reserva (id_cliente, id_habitacion, nombre_habitacion, fecha_checkin, fecha_checkout,     
+                            cantidad_huespedes, tipo_cama) 
+                            VALUES (:id_cliente, :id_habitacion, :roomtype, :checkin, :checkout, :persons, :bedtype)"""
+    conn.execute(text(query_insertar_reserva), {'id_cliente': id_cliente, 'id_habitacion': id_habitacion, 'roomtype': roomtype, 'checkin': checkin, 'checkout': checkout, 'persons': persons, 'bedtype': bedtype})
+
 @app.route('/generar_reservas', methods=['GET', 'POST'])
 def generar_reservas():
-
     conn = set_connection()
+
     data = request.json
 
     nombre = data.get('nombre')
@@ -39,11 +71,20 @@ def generar_reservas():
     bedtype = data.get('bedtype')
     
     try:
+        habitaciones_disponibles = verificar_disponibilidad_tipo(conn, roomtype)
+
+        habitaciones_libres = verificar_disponibilidad_fecha(conn, habitaciones_disponibles, checkin, checkout)
+
+        id_habitacion = habitaciones_libres[0]
+
         id_cliente = insertar_cliente(conn, nombre, apellido, identificacion, contact)
+
+        insertar_ocupacion(conn, id_habitacion, checkin, checkout)
+ 
+        insertar_reserva(conn, id_cliente, id_habitacion, roomtype, checkin, checkout, persons, bedtype)
 
         conn.commit()
         return jsonify({'message': 'Reserva realizada con éxito!'}), 201
-
     except Exception as e:
         return jsonify({'message': f'Error en el servidor: {str(e)}'}), 500
     finally:
@@ -116,8 +157,6 @@ def generar_reservas():
 #            entity['nombre_servicio'] = row.nombre_servicio
 #            entity['descripcion_servicio'] = row.descripcion_servicio
 #            data.append(entity)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=PORT)
