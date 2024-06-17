fa319fefbd4a9e0436becf5034070cffcebbeefc
JoelFiad <jfiad@fi.uba.ar>
2024-06-17 17:56:01 -0300

    Solucion de bug 405 6657f4bb9fa0d34168e6e21e

--- BackEnd-API/app.py
+++ BackEnd-API/app.py
@@ -182,7 +182,7 @@ def insertar_consulta(conn, name, contact, email, message):
                     VALUES (:name, :contact, :email, :message)"""
     conn.execute(text(query), {'name': name, 'contact': contact, 'email': email, 'message': message}) 
 
-@app.route('/crear_consulta')
+@app.route('/crear_consulta', methods=['POST'])
 def crear_consulta():
     conn = set_connection()
     try:
